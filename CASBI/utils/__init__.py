1427675410d119c2de631540d8eb1845e8dc47ef
vepe99 <viterbogiuseppe99@gmail.com>
2024-05-12 10:53:29 +0200

    import rescale

--- CASBI/utils/__init__.py
+++ CASBI/utils/__init__.py
@@ -1,4 +1,4 @@
-import CASBI.utils.create_dataframe
+from CASBI.utils.create_dataframe import rescale
 import CASBI.utils.create_file 
 import CASBI.utils.create_test_val_set
 import CASBI.utils.metrics
