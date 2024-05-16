ab5ea4340c36605d66c010a276ea938ffbf8d5f6
vepe99 <viterbogiuseppe1999@libero.it>
2024-05-16 10:24:35 +0200

    bug fix on load_train_objs

--- CASBI/utils/create_test_val_set.py
+++ CASBI/utils/create_test_val_set.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 
 """
 ==========================
@@ -10,7 +11,7 @@ divide the galaxies in 3 groups of mass, low (mass < 25 percentile), intermediat
 Then, for each group, select N galaxies equally spaced in infall time.
 
 """
-def get_even_space_sample(df_mass_masked, N=10):
+def get_even_space_sample(df_mass_masked, N=30):
     '''
     Given a dataframe of galaxy in a range of mass, it returns N equally infall time spaced samples  
     
@@ -30,14 +31,16 @@ def get_even_space_sample(df_mass_masked, N=10):
     index_val_time = np.linspace(0, len_infall_time-1, N)
     time = np.sort(df_mass_masked['infall_time'].unique())[index_val_time.astype(int)]
     for i, t in enumerate(time):
+        temp = df_mass_masked[df_mass_masked['infall_time']==t]
+        galaxy_temp = temp.sample(1)['Galaxy_name'].values[0]
         if i == 0:
-            df_time = df_mass_masked[df_mass_masked['infall_time']==t]
-        else:
-            df_time = pd.concat((df_time, df_mass_masked[df_mass_masked['infall_time']==t]) )
-            
+            df_time = df_mass_masked[df_mass_masked['Galaxy_name']==galaxy_temp]
+        else:  
+            df_galaxy = df_mass_masked[df_mass_masked['Galaxy_name']==galaxy_temp]
+            df_time = pd.concat([df_time, df_galaxy], ignore_index=True)
     return df_time
 
-def load_train_objs(df_path:str, test_path:str, train_path:None, val_path:None ) -> None:
+def load_train_objs(df_path:str, test_path=None, train_path=None, val_path=None ):
     """
     Create training, validation and test dataset. The test set is saved in a parquet file in the path test_path.
     The validation set is saved in a parquet file in the path val_path. The training set is saved in a parquet file in the path train_path.
@@ -59,7 +62,7 @@ def load_train_objs(df_path:str, test_path:str, train_path:None, val_path:None )
     high_mass = get_even_space_sample(train_set[train_set['star_log10mass']>=high_percentile_mass])
     val_set = pd.concat([low_mass, intermediate_mass, high_mass])
     if val_path is not None:
-        val_set.to_parquet(val_path)
+        val_set.to_parquet(os.path.join(val_path, 'val_data.parquet'))
     
     ### remove validation galaxies from train set
     train_set = train_set[~train_set['Galaxy_name'].isin(val_set['Galaxy_name'])]
@@ -71,14 +74,14 @@ def load_train_objs(df_path:str, test_path:str, train_path:None, val_path:None )
     high_mass = get_even_space_sample(train_set[train_set['star_log10mass']>=high_percentile_mass])
     test_set = pd.concat([low_mass, intermediate_mass, high_mass])
     
-    ### save the training set 
-    if test_set is not None:
-        test_set.to_parquet(test_path)
+    ### save the test set 
+    if test_path is not None:
+        test_set.to_parquet(os.path.join(test_path, 'test_data.parquet'))
     
     ### remove the test galaxies from the train set and saves it
     train_set = train_set[~train_set['Galaxy_name'].isin(test_set['Galaxy_name'])]
     if train_path is not None:
-        train_set.to_parquet(train_path)
+        train_set.to_parquet(os.path.join(train_path, 'training_data.parquet'))
     print('finish prepare data')
 
     return train_set, val_set, test_set
