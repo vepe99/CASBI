cbe9a3d14a0d1131274bdd4538c935b909d3f057
vepe99 <viterbogiuseppe1999@libero.it>
2024-05-29 22:05:58 +0200

    start working with optuna distributed and the test accuracy, tested mdd and c2st but are not suited for our case, better to use other metrics (MSE?)

--- notebooks/ltu_ili_test/test_2subhalos_experimental/run.py
+++ notebooks/ltu_ili_test/test_2subhalos_experimental/run.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import yaml
+import time
 import numpy as np
 import pandas as pd
 from multiprocessing import Pool
@@ -48,19 +49,36 @@ if __name__ == '__main__':
     write_to_yaml(repeat_minimum_theta, repeat_maximum_theta)
     print('write the right prior in the training.yaml file')
     
+    N_test = 10
+    def preprocess_testset(i):
+        galaxies = set(data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=i))
+        parameters =  data[data['Galaxy_name'].isin(galaxies)].drop(['feh', 'ofe', 'Galaxy_name'], axis=1).drop_duplicates().values.T.reshape(-1)
+        galaxy_data = data[data['Galaxy_name'].isin(galaxies)].values
+        histogram_galaxy, _, _ = np.histogram2d(galaxy_data[:, 0], galaxy_data[:, 1], bins=64, range=[[min_feh, max_feh], [min_ofe, max_ofe]])
+        sim_data =  np.expand_dims(np.log10(histogram_galaxy + 1e-6 +1), axis=0)
+        return parameters, sim_data, galaxies
     
-    galaxies_0 = data['Galaxy_name'].sample(N_subhalos)
+    # Create a pool of workers
+    with Pool() as pool:
+        # Map the function to the data
+        results = pool.map(preprocess_testset, range(N_test))
+        
+    # Unpack the results
+    theta_test, x_test, galaxies_test = zip(*results)
+    
+    #take the first test set element as x_0 and theta_0    
+    galaxies_0 = galaxies_test[0]
     data_to_plot_halos = data[data['Galaxy_name'].isin(galaxies_0)].to_parquet('./halos_0.parquet')
-    theta_0 =  data[data['Galaxy_name'].isin(galaxies_0)].drop(['feh', 'ofe', 'Galaxy_name'], axis=1).drop_duplicates().values.T.reshape(-1)
-    galaxy_data = data[data['Galaxy_name'].isin(galaxies_0)].values
-    histogram_galaxy, _, _ = np.histogram2d(galaxy_data[:, 0], galaxy_data[:, 1], bins=64, range=[[min_feh, max_feh], [min_ofe, max_ofe]])
-    x_0 =  np.expand_dims(np.log10(histogram_galaxy + 1e-6 +1), axis=0)
+    theta_0 =  theta_test[0]
+    x_0 =  x_test[0]
 
-    N = 10_000
+    N = 50_000
     def process_sample(i):
-        galaxies = data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=i)
-        while set(galaxies) == set(galaxies_0):
+        galaxies = data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=i+int(time.time()))
+        while (any(set(galaxies) == galaxy_in_testset for galaxy_in_testset in galaxies_test)):
             print('matched galaxies, try again')
+            print('galaxies', set(galaxies))
+            print('test galaxies', galaxies_test)
             galaxies = data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=i)
         parameters =  data[data['Galaxy_name'].isin(galaxies)].drop(['feh', 'ofe', 'Galaxy_name'], axis=1).drop_duplicates().values.T.reshape(-1)
         galaxy_data = data[data['Galaxy_name'].isin(galaxies)].values
@@ -76,7 +94,9 @@ if __name__ == '__main__':
     # Unpack the results
     theta, x = zip(*results)
     
-    #save in .npy files 
+    #save in .npy files, we remove the first element of the test set since it will be stored as x_0 and theta_0
+    np.save('./x_test.npy', x_test[1:])
+    np.save('./theta_test.npy', theta_test[1:])
     np.save('./x_0.npy', x_0)
     np.save('./theta_0.npy', theta_0)
     np.save('./x.npy', x)
