bad47b317d1395be4cbf53e2b6d6a1b7dd3e2d53
vepe99 <viterbogiuseppe1999@libero.it>
2024-06-10 21:00:51 +0200

    add the function to generate halos from histogram data with all the counts

--- CASBI/sbi/gen_halo.py
+++ CASBI/sbi/gen_halo.py
@@ -52,10 +52,7 @@ def gen_onehalo(data, N_subhalos, train:bool, galaxies_test:np.array, min_feh, m
     galaxies (np.array): list of galaxies
     
     """
-    if train==False:
-        galaxies = set(data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=int(time.time())))
-    else:
-        galaxies = set(data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=int(time.time())))
+    galaxies = set(data['Galaxy_name'].drop_duplicates().sample(N_subhalos, random_state=int(time.time())))
     if train: #if training check wheter or not those galaxy are present in the galaxy test set 
         while (any(set(galaxies) == galaxy_in_testset for galaxy_in_testset in galaxies_test)):
             print('matched galaxies, try again')
@@ -230,4 +227,139 @@ def gen_halo(data:pd.DataFrame, output_dir:str, training_yaml:str, n_test:int, n
     np.save(os.path.join(output_dir, './galaxy_training.npy'), galaxies)
     print('finish prepare the data')
     return N_subhalos_test, theta_test, x_test, galaxies_test, N_subhalos, theta, x, galaxies
+
+
+
+"""
+==============
+histogram data
+==============
+"""
+import random
+
+def gen_onehalo_hist(data_dir, N_subhalos, train:bool, galaxies_test:np.array ):
+    """
+    """
+    random.seed(int(time.time()))
+    galaxies = set(random.sample(os.listdir(data_dir), N_subhalos))
+    if train: #if training check wheter or not those galaxy are present in the galaxy test set 
+        while (any(set(galaxies) == galaxy_in_testset for galaxy_in_testset in galaxies_test)):
+            print('matched galaxies, try again')
+            print('galaxies', set(galaxies))
+            print('test galaxies', galaxies_test)
+            galaxies = set(random.sample(os.listdir(data_dir), N_subhalos))
+    parameters =  np.array([np.load(g)['star_log10mass', 'dm_log10mass', 'infall_time'] for g in galaxies ]).T
+    sorted_index = np.argsort(parameters[0], )[::-1] #orders the parameters in descending order of star mass
+    parameters = (parameters[:,sorted_index]).reshape(-1)
+    sim_data =  np.sum(np.array([np.load(g)['observables'] for g in galaxies ]))
+    return N_subhalos, parameters, sim_data, galaxies
+
+
+def gen_halo_Nsubhalos_hist(data_dir:pd.DataFrame, output_dir:str, n_test:int, n_train:int, max_subhalos:int=100, ):
+    """
+    """
     
+    arr = np.concatenate([np.repeat(i, n_test) for i in range(2, max_subhalos)]) #generate n_test samples for each number of subhalos from 2 to max_subhalos
+    np.random.shuffle(arr)
+    # Create a pool of workers
+    with Pool() as pool:
+        # Map the function to the data
+        results = pool.starmap(gen_onehalo_hist, [(data_dir, n_subhalos, 0, None) for n_subhalos in arr]) #n_subhalos is passed also as random state to generate the subhalos
+        
+    # Unpack the results
+    N_subhalos_test, parameters_test, x_test, galaxies_test = zip(*results)
+    N_subhalos_test = np.array(N_subhalos_test).reshape((len(N_subhalos_test), 1))  #needed for sbi input shape 
+
+    #take the first test set element as x_0 and theta_0    
+    galaxies_0 = galaxies_test[0]
+    # data_to_plot_halos = data[data['Galaxy_name'].isin(galaxies_0)].to_parquet('./halos_0.parquet')
+    N_subhalos_0 = N_subhalos_test[0]
+    x_0 =  x_test[0]
+        
+    arr = np.concatenate([np.repeat(i, n_train) for i in range(2, max_subhalos)]) #generate n_train samples for each number of subhalos from 2 to max_subhalos
+    np.random.shuffle(arr)
+    # Create a pool of workers
+    with Pool() as pool:
+        # Map the function to the data
+        results = pool.starmap(gen_onehalo, [(data_dir, n_subhalos, 1, galaxies_test) for n_subhalos in arr]) #n_subhalos is passed also as random state to generate the subhalos
+
+    # Unpack the results
+    N_subhalos, parameters, x, galaxies_training = zip(*results)
+    N_subhalos = np.array(N_subhalos).reshape((len(N_subhalos), 1))
+
+    #save in .npy files, we remove the first element of the test set since it will be stored as x_0 and theta_0')
+    np.save(os.path.join(output_dir,'x_test.npy'), x_test[1:])
+    np.save(os.path.join(output_dir,'N_subhalos_test.npy'), N_subhalos_test[1:])
+    np.save(os.path.join(output_dir, 'galaxy_test.npy'), galaxies_test[1:])
+    np.save(os.path.join(output_dir,'x_0.npy'), x_0)
+    np.save(os.path.join(output_dir,'N_subhalos_0.npy'), N_subhalos_0)
+    np.save(os.path.join(output_dir,'parameters_0.npy'), parameters_test[0])
+    np.save(os.path.join(output_dir,'galaxy_0.npy'), galaxies_0)
+    np.save(os.path.join(output_dir,'x.npy'), x)
+    np.save(os.path.join(output_dir,'N_subhalos.npy'), N_subhalos)
+    np.save(os.path.join(output_dir, 'galaxy_training.npy'), galaxies_training)
+    print('finish prepare the data')
+    
+    return N_subhalos_test, parameters_test, x_test, galaxies_test, N_subhalos_0, x_0, N_subhalos, parameters, x, galaxies_training
+
+def gen_halo_hist(data_dir:pd.DataFrame, output_dir:str, training_yaml:str, n_test:int, n_train:int, N_subhalos:int, prior_limits):
+    """
+    """
+    print('running gen halo')
+    
+    ####update the prior to accomodate for the right number of subhalos
+    minimum_theta = [prior_limits[col].values.min() for col in prior_limits.columns]   
+    maximum_theta = [prior_limits[col].values.max() for col in prior_limits.columns]       
+    minimum_theta = np.array(minimum_theta)
+    maximum_theta = np.array(maximum_theta)
+
+    repeat_minimum_theta = np.repeat(minimum_theta, N_subhalos)
+    repeat_maximum_theta = np.repeat(maximum_theta, N_subhalos) 
+
+    with open(training_yaml, 'r') as file:
+        yaml_file = yaml.safe_load(file)
+
+    repeat_minimum_theta = repeat_minimum_theta.tolist()
+    repeat_maximum_theta = repeat_maximum_theta.tolist()
+    # Update the value
+    yaml_file['prior']['args']['low'] = repeat_minimum_theta
+    yaml_file['prior']['args']['high'] = repeat_maximum_theta
+
+    # Write the data back to the file
+    with open(training_yaml, 'w') as file:
+        yaml.safe_dump(yaml_file, file)
+            
+    print('write the right prior in the training.yaml file')
+    
+    # Create a pool of workers
+    with Pool() as pool:
+        # Map the function to the data
+        results = pool.starmap(gen_onehalo, [(data_dir, N_subhalos, 0, None ) for i in range(n_test)]) #the index i is passed as random state to generate the subhalos
+        
+    # Unpack the results
+    N_subhalos_test, theta_test, x_test, galaxies_test = zip(*results)
+    
+    #take the first test set element as x_0 and theta_0    
+    galaxies_0 = galaxies_test[0]
+    theta_0 =  theta_test[0]
+    x_0 =  x_test[0]
+
+    # Create a pool of workers
+    with Pool() as pool:
+        # Map the function to the data
+        results = pool.starmap(gen_onehalo, [(data_dir, N_subhalos, 1, galaxies_test, ) for i in range(n_train)]) #the index i is passed as random state to generate the subhalos
+    # Unpack the results
+    N_subhalos, theta, x, galaxies = zip(*results)
+    
+    #save in .npy files, we remove the first element of the test set since it will be stored as x_0 and theta_0
+    np.save(os.path.join(output_dir,'./x_test.npy'), x_test[1:])
+    np.save(os.path.join(output_dir, './theta_test.npy'), theta_test[1:])
+    np.save(os.path.join(output_dir, './galaxy_test.npy'), galaxies_test[1:])
+    np.save(os.path.join(output_dir,'./x_0.npy'), x_0)
+    np.save(os.path.join(output_dir,'./theta_0.npy'), theta_0)
+    np.save(os.path.join(output_dir,'./galaxies_0.npy'), galaxies_0)
+    np.save(os.path.join(output_dir,'./x.npy'), x)
+    np.save(os.path.join(output_dir,'./theta.npy'), theta)
+    np.save(os.path.join(output_dir, './galaxy_training.npy'), galaxies)
+    print('finish prepare the data')
+    return N_subhalos_test, theta_test, x_test, galaxies_test, N_subhalos, theta, x, galaxies
--- notebooks/ltu_ili_test/n_subhalo_finder/CNN.py
+++ notebooks/ltu_ili_test/n_subhalo_finder/CNN.py
@@ -31,3 +31,31 @@ class ConvNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.fc_layers(out)
         return out
+
+class ConvNet_2(nn.Module):
+    def __init__(self, input_channel, output_dim):
+        super(ConvNet, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(input_channel, 8, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(32 * 8 * 8, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_dim),
+        )
+
+    def forward(self, x):
+        out = self.conv_layers(x)
+        out = out.view(out.size(0), -1)
+        out = self.fc_layers(out)
+        return out
